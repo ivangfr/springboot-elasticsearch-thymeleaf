c33470d0315554f27dc6ae0f2cbc0548aeac9f14
Ivan Franchin <ivan.franchin@takeaway.com>
2018-09-22 10:01:09 +0200

    changes - add created field to review; - add cancel button to create and edit product forms; - refactor html files.

--- product-api/src/main/java/com/mycompany/productapi/model/Review.java
+++ product-api/src/main/java/com/mycompany/productapi/model/Review.java
@@ -1,11 +1,17 @@
 package com.mycompany.productapi.model;
 
+import com.mycompany.productapi.util.DateTimeUtil;
 import lombok.Data;
 
 @Data
 public class Review {
 
+    public Review() {
+        this.created = DateTimeUtil.createCurrentDateAsString();
+    }
+
     private String comment;
     private Short stars;
+    private String created;
 
 }
--- product-ui/src/main/java/com/mycompany/productui/client/dto/Review.java
+++ product-ui/src/main/java/com/mycompany/productui/client/dto/Review.java
@@ -2,10 +2,13 @@ package com.mycompany.productui.client.dto;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class Review {
 
     private String comment;
     private Short stars;
+    private Date created;
 
 }
