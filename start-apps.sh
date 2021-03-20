62308094a5231e52022def4871badf9f40f9d785
Ivan Franchin <ivan.franchin@takeaway.com>
2021-03-20 17:52:52 +0000

    project update - convert application.yml files to application.properties files; - add my-functions.sh with function wait_for_container_log; - refactor pom.xml files, adding jib problem in the main pom.xml; - using openjdk version 11.0.10; - update README.

--- start-apps.sh
+++ start-apps.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+source scripts/my-functions.sh
+
 echo
 echo "Starting eureka..."
 
 docker run -d --rm --name eureka \
   -p 8761:8761 \
   --network springboot-elasticsearch-thymeleaf_default \
+  --health-cmd="curl -f http://localhost:8761/actuator/health || exit 1" --health-start-period=30s \
   docker.mycompany.com/eureka-server:1.0.0
 
-sleep 2
+wait_for_container_log "eureka" "Started"
 
 echo
 echo "Starting product-api..."
@@ -17,9 +20,10 @@ docker run -d --rm --name product-api \
   -p 9080:8080 \
   -e EUREKA_HOST=eureka -e ELASTICSEARCH_HOST=elasticsearch \
   --network springboot-elasticsearch-thymeleaf_default \
+  --health-cmd="curl -f http://localhost:8080/actuator/health || exit 1" --health-start-period=30s \
   docker.mycompany.com/product-api:1.0.0
 
-sleep 2
+wait_for_container_log "product-api" "Started"
 
 echo
 echo "Starting product-ui..."
@@ -28,4 +32,15 @@ docker run -d --rm --name product-ui \
   -p 9081:8080 \
   -e EUREKA_HOST=eureka \
   --network springboot-elasticsearch-thymeleaf_default \
+  --health-cmd="curl -f http://localhost:8080/actuator/health || exit 1" --health-start-period=30s \
   docker.mycompany.com/product-ui:1.0.0
+
+wait_for_container_log "product-ui" "Started"
+
+printf "\n"
+printf "%12s | %37s |\n" "Application" "URL"
+printf "%12s + %37s |\n" "------------" "-------------------------------------"
+printf "%12s | %37s |\n" "eureka" "http://localhost:8761"
+printf "%12s | %37s |\n" "product-api" "http://localhost:9080/swagger-ui.html"
+printf "%12s | %37s |\n" "product-ui" "http://localhost:9081"
+printf "\n"
