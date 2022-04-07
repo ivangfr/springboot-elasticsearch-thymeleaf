4b2455d4aa56a409c4e55c4505754b97cb2034e7
Ivan Franchin <ivan.franchin@takeaway.com>
2022-04-07 17:11:05 +0200

    project upgrade - upgrade to spring-boot 2.6.6; - upgrade to jib 3.2.1; - upgrade to openjdk-image 11.0.14.1; - remove native option in docker-build script and README.

--- docker-build.sh
+++ docker-build.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-#if [ "$1" = "native" ];
-#then
-#  ./mvnw clean spring-boot:build-image --projects eureka-server -DskipTests
-#  ./mvnw clean spring-boot:build-image --projects product-api -DskipTests
-#  ./mvnw clean spring-boot:build-image --projects product-ui -DskipTests
-#else
-  ./mvnw clean compile jib:dockerBuild --projects eureka-server
-  ./mvnw clean compile jib:dockerBuild --projects product-api
-  ./mvnw clean compile jib:dockerBuild --projects product-ui
-#fi
+./mvnw clean compile jib:dockerBuild --projects eureka-server
+./mvnw clean compile jib:dockerBuild --projects product-api
+./mvnw clean compile jib:dockerBuild --projects product-ui
