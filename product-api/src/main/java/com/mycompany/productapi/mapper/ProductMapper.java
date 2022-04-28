2839d74b37627ddfeb0f52c954f0dde36ed15cb3
Ivan Franchin <ivan.franchin@takeaway.com>
2022-04-28 16:21:17 +0100

    project upgrade - upgrade to spring-boot 2.6.7; - upgrade to springdoc-openapi 1.6.8; - upgrade to spring-boot 2.6.7; - add Mapping annotation to ignore some fields in mapper class.

--- product-api/src/main/java/com/mycompany/productapi/mapper/ProductMapper.java
+++ product-api/src/main/java/com/mycompany/productapi/mapper/ProductMapper.java
@@ -6,6 +6,7 @@ import com.mycompany.productapi.rest.dto.AddReviewRequest;
 import com.mycompany.productapi.rest.dto.CreateProductRequest;
 import com.mycompany.productapi.rest.dto.UpdateProductRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
@@ -15,9 +16,18 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 )
 public interface ProductMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "reference", ignore = true)
+    @Mapping(target = "reviews", ignore = true)
+    @Mapping(target = "created", ignore = true)
     Product toProduct(CreateProductRequest createProductRequest);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "reference", ignore = true)
+    @Mapping(target = "reviews", ignore = true)
+    @Mapping(target = "created", ignore = true)
     void updateProductFromRequest(UpdateProductRequest updateProductRequest, @MappingTarget Product product);
 
+    @Mapping(target = "created", ignore = true)
     Review toReview(AddReviewRequest addReviewRequest);
 }
