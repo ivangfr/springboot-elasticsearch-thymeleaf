0e910d47cc673a082dc3282cc93e40c047a28e3d
Ivan Franchin <ivan.franchin@takeaway.com>
2025-04-18 13:55:24 +0200

    Project upgrade
    - Upgrade to Spring Boot 3.4.4
    - Upgrade to Jib 3.4.5
    - Upgrade to SpringDoc OpenAPI 2.8.6
    - Upgrade to Elasticsearch Docker image 8.15.5
    - Add the .gitattributes file
    - Remove ProductService interface and just kept the implementation class
    - Update README file

--- product-api/src/main/java/com/ivanfranchin/productapi/product/ProductService.java
+++ product-api/src/main/java/com/ivanfranchin/productapi/product/ProductService.java
@@ -1,18 +1,39 @@
 package com.ivanfranchin.productapi.product;
 
+import com.ivanfranchin.productapi.product.exception.ProductNotFoundException;
 import com.ivanfranchin.productapi.product.model.Product;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
-public interface ProductService {
+import static org.apache.commons.lang3.RandomStringUtils.secure;
 
-    Page<Product> listProductsByPage(Pageable pageable);
+@RequiredArgsConstructor
+@Service
+public class ProductService {
 
-    Product validateAndGetProductById(String id);
+    private final ProductRepository productRepository;
 
-    Product saveProduct(Product product);
+    public Page<Product> listProductsByPage(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
 
-    void deleteProduct(Product product);
+    public Product validateAndGetProductById(String id) {
+        return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
+    }
 
-    Page<Product> search(String text, Pageable pageable);
+    public Product saveProduct(Product product) {
+        product.setReference("SBES@%s-%s"
+                .formatted(secure().nextAlphanumeric(4), secure().nextNumeric(5)));
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Product product) {
+        productRepository.delete(product);
+    }
+
+    public Page<Product> search(String text, Pageable pageable) {
+        return productRepository.findByReferenceOrNameOrDescription(text, text, text, pageable);
+    }
 }
